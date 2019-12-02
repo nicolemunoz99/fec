286664708ea5841f7b8c65cb6bad2b75843dc8aa
Nicole Munoz <nicolemunoz@protonmail.com>
2019-12-02 15:09:59 -0700

    update nav buttons

--- src/components/overview/imageGallery/ExpandedView.jsx
+++ src/components/overview/imageGallery/ExpandedView.jsx
@@ -57,8 +57,12 @@ class ExpandedView extends React.Component {
                     className="expanded-view-photo" src={this.props.photos[this.props.currentPhotoIndex].url}>
               </img>
             </div>
-            <div onClick={this.props.photoNavHandler} className="photo-nav-left">Left</div>
-            <div onClick={this.props.photoNavHandler} className="photo-nav-right">Right</div>
+            <div onClick={this.props.photoNavHandler} className="photo-nav-left">
+              <i class="far fa-arrow-alt-circle-left fa-2x nav-bg"></i>
+            </div>
+            <div onClick={this.props.photoNavHandler} className="photo-nav-right">
+              <i class="far fa-arrow-alt-circle-right fa-2x nav-bg"></i>
+            </div>
           </div>
         </div>
         {/* thumbnails */}
