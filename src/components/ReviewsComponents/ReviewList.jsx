3d9c492c1269e56d8fd352feef01061b00499f71
Joshua Smith <lopper99@hotmail.com>
2019-12-03 18:22:19 -0700

    merge and commit

--- src/components/ReviewsComponents/ReviewList.jsx
+++ src/components/ReviewsComponents/ReviewList.jsx
@@ -2,37 +2,62 @@ import React from 'react';
 import ReviewCard from './ReviewCard.jsx';
 
 const ReviewList = (props) => {
-  console.log(props);
-  let moreButton = '';
+  // console.log(props);
+  let moreButton;
   if (props.reviews.length > 2) {
-    moreButton = `<button onClick={(e) => props.update({ more: !props.more })}>{props.more ? 'LESS REVIEWS' : 'MORE REVIEWS'}</button>`;
+    moreButton = (<button onClick={(e) => props.update({ more: !props.more })}>{props.more ? 'LESS REVIEWS' : 'MORE REVIEWS'}</button>);
   }
-  let modal = document.getElementById('rmodal');
   window.onclick = (e) => {
-    if (e.target === modal) {
-      modal.style.display = 'none';
+    if (e.target === document.getElementById('rmodal')) {
+      document.getElementById('rmodal').style.display = 'none';
     }
   }
-
-  let getChristics = (chars) => {
-    let christics = '';
-    if (chars.hasOwnProperty('Size')) {
-      let sizeMeaning = ['A size too small', '1/2 a size too small', 'Perfect', '1/2 a size too big', 'A size too big'];
-      let index;
-      let radios = document.getElementsByName('size');
-      if (radios) {
-        for (let i = 0; i < radios.length; ++i) {
-          if (radios[i].checked) {
-            index = radios[i].value + 1;
-          }
+  let christics = [];
+  let createChars = (char, meaning) => {
+    let index;
+    let radios = document.getElementsByName(char);
+    if (radios) {
+      for (let i = 0; i < radios.length; ++i) {
+        if (radios[i].checked) {
+          index = Number(radios[i].value) - 1;
         }
       }
-      christics += `<div id='size-indicator'>${index ? sizeMeaning[index] : 'None selected'}</div>Size<input type='radio' name='size' value='1'></input><label htmlFor='1'>1</label><input type='radio' name='size' value='2'></input><label htmlFor='2'>2</label><input type='radio' name='size' value='3'></input><label htmlFor='3'>3</label><input type='radio' name='size' value='4'></input><label htmlFor='4'>4</label><input type='radio' name='size' value='5'></input><label htmlFor='5'>5</label><div id='size1'>${sizeMeaning[0]}</div><div id='size5'>${sizeMeaning[4]}</div>`;
     }
-    // if (christics.length) {
-  
-    // }
-    return christics;
+    christics.push(
+      <div id={char + '-char'}>
+        {char}: <div id={char + '-indicator'}>{index !== undefined ? meaning[index] : 'None selected'}</div>
+        <input type='radio' name={char} value='1' onChange={(e) => props.force()}></input>
+        <label htmlFor='1'>1</label>
+        <input type='radio' name={char} value='2' onChange={(e) => props.force()}></input>
+        <label htmlFor='2'>2</label>
+        <input type='radio' name={char} value='3' onChange={(e) => props.force()}></input>
+        <label htmlFor='3'>3</label>
+        <input type='radio' name={char} value='4' onChange={(e) => props.force()}></input>
+        <label htmlFor='4'>4</label>
+        <input type='radio' name={char} value='5' onChange={(e) => props.force()}></input>
+        <label htmlFor='5'>5</label>
+        <div id='size1'>{meaning[0]}</div>
+        <div id='size5'>{meaning[4]}</div>
+      </div>
+    );
+  }
+  if (props.christics.hasOwnProperty('Size')) {
+    createChars('Size', ['A size too small', '1/2 a size too small', 'Perfect', '1/2 a size too big', 'A size too big'])
+  }
+  if (props.christics.hasOwnProperty('Length')) {
+    createChars('Length', ['Runs short', 'Runs slightly short', 'Perfect', 'Runs slightly long', 'Runs long']);
+  }
+  if (props.christics.hasOwnProperty('Width')) {
+    createChars('Width', ['Too narrow', 'Slightly narrow', 'Perfect', 'Slightly wide', 'Too wide']);
+  }
+  if (props.christics.hasOwnProperty('Fit')) {
+    createChars('Fit', ['Runs tight', 'Runs slightly tight', 'Perfect', 'Runs slightly long', 'Runs long']);
+  }
+  if (props.christics.hasOwnProperty('Comfort')) {
+    createChars('Comfort', ['Uncomfortable', 'Slightly uncomfortable', 'Okay', 'Comfortable', 'Perfect']);
+  }
+  if (props.christics.hasOwnProperty('Quality')) {
+    createChars('Quality', ['Poor', 'Below average', 'What I expected', 'Pretty great', 'Perfect']);
   }
 
   return (
@@ -45,17 +70,55 @@ const ReviewList = (props) => {
           return <ReviewCard review={review} helpful={props.helpful} report={props.report} key={i} />
         }
       })}
-      {moreButton}
+      {moreButton ? moreButton : ''}
       <button onClick={(e) => {
-        modal.style.display = 'block';
+        document.getElementById('rmodal').style.display = 'block';
       }}>ADD A REVIEW +</button>
       <div id="rmodal" className="rmodal">
         <div className="rmodal-content">
           <span className="rclose" onClick={(e) => {
-            modal.style.display = 'none';
+            document.getElementById('rmodal').style.display = 'none';
           }}>&times;</span>
           <h5>Write Your Review</h5>
-          <form>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            let review = {};
+            let rating = document.getElementsByName('rrating');
+            if (rating) {
+              for (let i = 0; i < rating.length; ++i) {
+                if (rating[i].checked) {
+                  review.rating = Number(rating[i].value);
+                }
+              }
+            }
+            review.summary = document.getElementById('rsummary').value;
+            review.body = document.getElementById('rbody').value;
+            let reco = document.getElementsByName('rrecommend');
+            if (reco) {
+              for (let i = 0; i < reco.length; ++i) {
+                if (reco[i].checked) {
+                  review.recommend = Boolean(reco[i].value);
+                }
+              }
+            }
+            review.name = document.getElementById('ruser').value;
+            review.email = document.getElementById('remail').value;
+            review.photos = [];
+            review.characteristics = {};
+            for (let key in props.christics) {
+              let chrating = document.getElementsByName(key);
+              for (let i = 0; i < chrating.length; ++i) {
+                if (chrating[i].checked) {
+                  let strKey = props.christics[key].id.toString();
+                  review.characteristics[strKey] = Number(chrating[i].value);
+                }
+              }
+            }
+
+            console.log(review);
+            props.submit(review);
+            document.getElementById('rmodal').style.display = 'none';
+          }}>
             Rating:
             <input type='radio' name='rrating' value='1'></input>
             <label htmlFor='1'>1</label>
@@ -67,18 +130,18 @@ const ReviewList = (props) => {
             <label htmlFor='4'>4</label>
             <input type='radio' name='rrating' value='5'></input>
             <label htmlFor='5'>5</label><br></br>
-            Recommended: <input type='radio' name='rrecommend' value='yes'></input>
+            Recommended: <input type='radio' name='rrecommend' value='true'></input>
             <label htmlFor='yes'>Yes</label>
-            <input type='radio' name='rrecommend' value='no'></input>
+            <input type='radio' name='rrecommend' value='false'></input>
             <label htmlFor='no'>No</label><br></br>
             Characteristics:<br></br>
-            {getChristics(props.christics)}
+            <div id='christics'>{christics}</div>
             Summary:<br></br>
-            <input type='text' id='rsummary' defaultValue={'About the' + props.pname} maxLength='60' autoFocus required></input><br></br>
+            <input type='text' id='rsummary' style={{ width: '15em' }} defaultValue={'About the ' + props.pname} maxLength='60' autoFocus required></input><br></br>
             Review body:<br></br>
             <textarea id='rbody' rows='4' placeholder='Why did you like the product or not?' maxLength='1000' required></textarea><br></br>
             Nickname:<br></br>
-            <input type='text' id='ruser' placeholder='Example: jackson11!'></input><br></br>
+            <input type='text' id='ruser' style={{ width: '15em' }} placeholder='Example: jackson11!'></input><br></br>
             Email:<br></br>
             <input type='text' id='remail' style={{ width: '15em' }} placeholder='Example: jackson11@email.com'></input><br></br>
             For authentication reasons, you will not be emailed <br></br>
